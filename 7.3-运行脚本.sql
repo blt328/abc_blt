e46206055ec9c4bc3d8a25f0b2794b978e5f0810
blt.zhou <43537203+blt328@users.noreply.github.com>
2019-06-30 22:08:10 +0800

    Create 7.3-运行脚本.sql

---
+++ 7.3-运行脚本.sql
@@ -0,0 +1,7 @@
+begin
+  -- Call the procedure
+  p_abc_fct_rr_dist(date '2019-06-01');
+  p_abc_fct_ra_dist(date '2019-06-01');
+  p_abc_fct_aa_dist(date '2019-06-01');
+  p_abc_fct_ao_dist(date '2019-06-01');
+end;
